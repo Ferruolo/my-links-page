694d84649a8a1c7d108caf010333362aa4966ae6
andrew ferruolo <andrew.ferruolo@gmail.com>
2024-06-01 12:43:59 -0400

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import "../styles/reset.css"
+import Head from "next/head";
+
+
+const MyApp = ({ Component, pageProps }) => {
+    return <>
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Andrew Ferruolo</title>
+            <link rel="icon" href="/favicon.ico"/>
+
+        </Head>
+        <Component {...pageProps} />
+    </>
+
+
+
+}
+export default MyApp
