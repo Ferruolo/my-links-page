b0449d9e26aea8bc085c5bd96c6db962693711c2
andrew ferruolo <andrew.ferruolo@gmail.com>
2024-06-01 14:27:29 -0400

    added contact info in plaintext

--- pages/ContactCard.js
+++ pages/ContactCard.js
@@ -1,11 +1,33 @@
 import styles from "../styles/Home.module.css";
 import {Major_Mono_Display} from "next/font/google";
+import Image from "next/image";
+
 
 const majorMono = Major_Mono_Display({weight: '400', preload: false})
 
 export default function ContactCard() {
     return <main className={`${styles.container} ${majorMono.className}`}>
-
+        {/*Upper Bar, Name and Headshot*/}
+        <div className={styles.profilePicBox}>
+            <Image src="/Headshot.jpeg"
+                   alt={"Picture of me"}
+                   width={100}
+                   height={100}
+                   className={styles.profilePicImg}
+            />
+        </div>
+        <div className={styles.contactsName}>
+            Andrew Ferruolo
+        </div>
+        <div className={styles.contactLineItem}>
+            phone: (860)-999-4565
+        </div>
+        <div className={styles.contactLineItem}>
+            personal email: andrew.ferruolo@gmail.com
+        </div>
+        <div className={styles.contactLineItem}>
+            umich email: <br></br>ferruolo@umich.edu
+        </div>
     </main>
 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import styles from '../styles/Home.module.css';
 import { Major_Mono_Display } from 'next/font/google'
 import {useState} from "react";
+import {useRouter} from "next/router";
 
 
 const majorMono = Major_Mono_Display({weight: '400', preload: false})
 
 
 const LinkButton = ({text, link}) => {
-    const redirect = () => {
-        window.location.assign(link)
+    const router = useRouter();
+
+    const redirect = e => {
+        e.preventDefault();
+        router.push(link);
     }
 
     return <button className={styles.linkButton} onClick={redirect}>
         {text}
     </button>
 }
-
 
 
 export default function Home() {
@@ -29,7 +32,8 @@ export default function Home() {
           </div>
           <LinkButton text={'LinkedIn'} link={'https://linkedin.com/in/andrew-ferruolo'}/>
           <LinkButton text={'X.com'} link={'https://x.com/FerruoloA'}/>
-          
+          <LinkButton text={'contact'} link={'ContactCard'} />
+
 
 
       </main>
